d52a6ffb499e4058e7791dbb6fbe5336da455595
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-09-02 16:05:32 -0300

    DIP

--- src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
+++ src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
@@ -1,12 +1,22 @@
 package br.com.mariojp.solid.dip;
 
 public class EmailNotifier {
-	private final SmtpClient smtp = new SmtpClient();
 
-	public void welcome(User user) {
-		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
-			// Estado inicial: ainda assim usa SMTP real (bug proposital)
-		}
-		smtp.send(user.email(), "Bem-vindo", "Olá " + user.name());
+	private final MailSender mailSender;
+
+	public EmailNotifier() {
+		this(new SmtpClient());
 	}
+
+    public EmailNotifier(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void welcome(User user) {
+		if (!"true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
+			mailSender.enviar(user.email(), "Bem-vindo", "Olá " + user.name());
+		}
+	}
+
+
 }
--- src/main/java/br/com/mariojp/solid/dip/SmtpClient.java
+++ src/main/java/br/com/mariojp/solid/dip/SmtpClient.java
@@ -1,11 +1,10 @@
 package br.com.mariojp.solid.dip;
 
-public class SmtpClient {
-    public void send(String to, String subject, String body){
-        if(!"true".equalsIgnoreCase(System.getProperty("SMTP_AVAILABLE"))) {
-            throw new IllegalStateException("SMTP indisponível");
-        }
-        if(to == null || !to.contains("@")) throw new IllegalArgumentException("email inválido");
-        // envia ...
+public class SmtpClient implements MailSender{
+    @Override
+    public void enviar(String to, String subject, String body) {
+        System.out.println("Enviando e-mail de verdade para: " + to);
+        System.out.println("Assunto: " + subject);
+        System.out.println("Corpo: " + body);
     }
 }
